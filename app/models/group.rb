4cb656697b46a3de010c3e4aa7174160d8efa274
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-08 23:54:27 +0000

    try to add mailer-function

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,8 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :introduction, presence: true
   has_one_attached :image
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 end
